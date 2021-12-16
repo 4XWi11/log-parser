a103845c0f4934d5add612fc587bb4b81bf614c7
Scr1pt <56531005+Scr1pt-kid@users.noreply.github.com>
2021-12-16 19:13:34 +0800

    Add files via upload

--- lib/LogParser.py
+++ lib/LogParser.py
@@ -2,6 +2,8 @@ from lib.user import NginxVisitor
 import re
 from config import *
 import time
+import requests
+import json
 
 class LogParser:
     def __init__(self, log_path, regex):
@@ -21,12 +23,12 @@ class LogParser:
         此函数用来对日志进行分割，然后针对每个ip创建访问者对象，并写入 self.visitor属性
         :return:
         """
+        table = ['ip', 'time', 'method_and_uri', 'status', 'content-length', '', 'user_agent']
+        time_table = []
+
         for line in self.content:
             match_group = re.findall(self.regex, line)
-            """
-                print(match_group,len(match_group),type(match_group))
-                print(type(match_group[0]),len(match_group[0]))
-            """
+
             tempDic = {}
 
             try:
@@ -35,7 +37,6 @@ class LogParser:
                 print('[❌] error logs:',line)
                 continue
 
-            table = ['ip','time','method_and_uri','status','content-length','','user_agent']
             for i in range(1,length):
                 try:
                     tempDic[table[i]] = match_group[0][i]
@@ -49,13 +50,33 @@ class LogParser:
             else:
                 self.ipList[ip].append(tempDic)
 
-        # print(self.ipList)
+    def ip_api(self,ip):
+        """ Get IP information from API """
+        url = f'http://ip-api.com/json/{ip}'
+        result = requests.get(url, timeout=5)
+        ip_data = json.loads(result.text)
+
+        if ip_data['status'] == 'success':
+            self.ip_country = ip_data['country']
+            self.ip_isp = ip_data['isp']
+            self.ip_city = ip_data['city']
+
+        self.ip_inf_output()
+        return
+
+    def ip_inf_output(self):
+        print("[✅]country:",self.ip_country,'|',
+              "isp:",self.ip_isp,'｜'
+              "city:",self.ip_city)
+        return
+
+
 
     def time_log(self):
         time_local = time.localtime(time.time())
 
-        start_time = "2021-08-14 00:00:00"
-        end_time = "2021-08-15 00:00:00"
+        start_time = "2021-08-04 00:00:00"
+        end_time = "2021-08-05 00:00:00"
 
         start_time = time.strptime(start_time, "%Y-%m-%d %H:%M:%S")
         start_time = int(time.mktime(start_time))
@@ -63,13 +84,11 @@ class LogParser:
         end_time = time.strptime(end_time, "%Y-%m-%d %H:%M:%S")
         end_time = int(time.mktime(end_time))
 
-        self.judge_ddos(start_time,end_time)
+        self.judge_attack(start_time,end_time)
 
         return
-    def judge_ddos(self,start_time,end_time):
+    def judge_attack(self,start_time,end_time):
         # 只需要记录每小时即可，其他数据无意义。
-
-        total_time = []
 
         month_dict = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5,
                            'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10,
@@ -77,28 +96,25 @@ class LogParser:
 
         hour_count = 0
         for time_loop in range(int(start_time),end_time,3600):
-            for ip in self.ipList.keys():
-                for item in self.ipList[ip]:
-                    # 分离数据
-                    day = item['time'][0:2]
-                    mon = month_dict[item['time'][3:6]]
-                    yr = item['time'][7:11]
-                    hr = item['time'][12:14]
-                    minute = item['time'][15:17]
-                    sec = item['time'][18:20]
+            for item in self.ipList.values():
+                for indice in range(len(item)):
+                    day = item[indice]['time'][0:2]
+                    mon = month_dict[item[indice]['time'][3:6]]
+                    yr = item[indice]['time'][7:11]
+                    hr = item[indice]['time'][12:14]
+                    minute = item[indice]['time'][15:17]
+                    sec = item[indice]['time'][18:20]
 
                     # 构建时间戳
                     temp_time = ('%s-%s-%s %s:%s:%s')%(yr,mon,day,hr,minute,sec)
                     timeArray = time.strptime(temp_time, "%Y-%m-%d %H:%M:%S")
                     timestamp = time.mktime(timeArray)
-                    total_time.append(timestamp)
 
-                    # print(timestamp)
                     # 统计每小时访问量
                     if timestamp in range(time_loop,time_loop+3600):
                         hour_count += 1
 
-            # 转换成时间戳并输出
+            # 转换成日期并输出
             out_time = time.localtime(time_loop)
             out_time = time.strftime("%Y-%m-%d %H:%M:%S", out_time)
             print(f"[✅]{out_time} count : {hour_count}")
@@ -109,8 +125,15 @@ class LogParser:
         此函数用来对self.visitor属性进行检查，比如统计带有攻击行为的ip
         :return:
         """
-        return 123
+        urlLis = []
 
+        sensive_word_dic = ['shell','backdoor','$',r'../']
+        for item in self.ipList.values():
+            url = item[0]['method_and_uri']
+            for sensive_word_item in sensive_word_dic:
+                if sensive_word_item in url:
+                    print(f'[❌] May be the Attacker !!! {url}')
+        return True
     def save_result(self):
         """
         此函数用来按照格式生成统计日志
@@ -119,8 +142,6 @@ class LogParser:
         self.time_log()
 
         return
-
-
     def run(self):
         self.parser()
         self.check()

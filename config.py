c165b3012e6b7c04b7f637de7cc902274b901cc4
Scr1pt <56531005+Scr1pt-kid@users.noreply.github.com>
2021-12-16 20:48:31 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 
 #匹配日志的正则
 REGEX_LOG_NGINX = r"(.*)\s-\s-\s\[(.*)]\s\"(.*)\"\s(\d+|-)\s(\d+|-)\s\"(.*)\"\s\"(.*)\""
-REGEX_LOG_APACHE = r""
+REGEX_LOG_APACHE = r"(.*)\s-\s-\s\[(.*)]\s\"(.*)\"\s(\d+|-)\s(\d+|-)\s\"(.*)\"\s\"(.*)\""
 
-REGEX = [REGEX_LOG_NGINX,REGEX_LOG_APACHE
-]
+REGEX = [REGEX_LOG_NGINX,REGEX_LOG_APACHE]
 #生成结果报告
 
 RESULT_LOG = True

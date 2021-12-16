ddfc9f8a9557b5503ceebdb1acb9eaa735b424ff
Scr1pt <56531005+Scr1pt-kid@users.noreply.github.com>
2021-12-16 16:13:09 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from config import REGEX
 
 
 def main():
+    print('''_   _                     _                                               
+| | | |_____ __  _   _    | |    ___   __ _      _ __   __ _ _ __ ___  ___  
+| |_| |_  / '_ \| | | |   | |   / _ \ / _` |    | '_ \ / _` | '__/ __|/ _ \\
+|  _  |/ /| | | | |_| |   | |__| (_) | (_| |    | |_) | (_| | |  \__ \  __/
+|_| |_/___|_| |_|\__,_|___|_____\___/ \__, |____| .__/ \__,_|_|  |___/\___| 
+                     |_____|          |___/_____|_|                         
+''')
     arg = argparse.ArgumentParser()
     arg.add_argument("--path", help="The log you ready to analyse.", type=str)
     arg.add_argument("--type", help="0->nginx_log/ 1->apache_log", type=int)
@@ -11,7 +18,8 @@ def main():
     log_type = arg.parse_args().type
     log_parser_new = LogParser(path, REGEX[log_type])
     log_parser_new.run()
-    # print()
+    print()
 
 if __name__ == '__main__':
     main()
+
